fde263134beb59b66860c6e8950ef1c292a003ce
N C <>
2019-10-06 13:51:53 -0700

    editing

--- HistogramGenerator.java
+++ HistogramGenerator.java
@@ -5,8 +5,77 @@ import java.awt.Color;
 
 public class HistogramGenerator {
     
-    
+    public static void main(String[] args)
+    {
 
+    }
+
+
+    public static double[] process(int[] hist)
+    {
+        hist = removeZero(hist);
+        double[] h = new double[hist.length];
+        for(int i = 0; i < h.length; i++)
+        {
+            h[i] = (double)hist[i]/sum(hist);
+        }
+        return resize(h,100);
+    }
+    public static int[] removeZero(int[] arr)
+    {
+        int leftIdx = 0;
+        int rightIdx = arr.length;
+        boolean l = true;
+        boolean r = true;
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(l && leftIdx < arr.length && arr[leftIdx] == 0)
+            {
+                leftIdx++;
+            }else{
+                l = false;
+            }
+            if(r && rightIdx > 0 && arr[rightIdx-1] == 0)
+            {
+                rightIdx--;
+            }else{
+                r = false;
+            }
+        }
+        if(leftIdx < rightIdx)
+        {
+            return subarr(arr,leftIdx,rightIdx);
+        }
+        else{
+            return null;
+        }
+    }
+    public static int[] subarr(int[] arr, int min, int max)
+    {
+        int[] out = new int[max-min];
+        for(int i = min; i < max; i++)
+        {
+            out[i-min] = arr[i];
+        }
+        return out;
+    }
+    public static double[] resize(double[] in, int length)
+    {
+        double[] out = new double[length];
+        double scalar = (double)(in.length-1)/(out.length-1);
+        for(int i = 0; i < out.length; i++)
+        {
+            double tempIdx = scalar*i;
+            int minIdx = (int)Math.floor(tempIdx);
+            int maxIdx = (int)Math.ceil(tempIdx);
+            out[i] = linearInterpolation(in[minIdx],in[(maxIdx < in.length ? maxIdx : in.length-1)],tempIdx-minIdx);
+        }
+        return out;
+    }
+    public static double linearInterpolation(double min, double max, double percentage)
+    {
+        return (max-min)*percentage+min;
+    }
     public static double sum(double[] arr)
     {
         double sum = 0;
--- ImageUtil.java
+++ ImageUtil.java
@@ -1,106 +1,132 @@
 import java.util.*;
+
+import javax.imageio.ImageIO;
+
 import java.awt.image.*;
 import java.awt.Color;
+import java.io.*;
 
-public class ImageUtil
-{
+public class ImageUtil {
+    public static void main(String[] args) {
+        try {
+            BufferedImage bi = ImageIO.read(new File("test2.png"));
+            bi = grayscale(bi);
+            int threshold = (int) (averageColor(bi).getRed() * 0.5);
+            bi = thresholdFilter(bi, threshold);
+            ImageIO.write(bi, "png", new File("test2out.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Returns a sub-image from the BufferedImage parameter from [x1,x2) [y1,y2)
+     * 
      * @param bi The BufferedImage to extract the sub-image from
      * @param x1 The initial x value, must be less than x2
-     * @param x2 The final x value, where the last value of the output BufferedImage is the pixel x-1
+     * @param x2 The final x value, where the last value of the output BufferedImage
+     *           is the pixel x-1
      * @param y1 The initial y value, must be less than y2
-     * @param y2 The final y value, where the last value of the output BufferedImage is the pixel y-1
-     * @return The sub-image extracted from the input from x1 to x2(not inclusive) and y2 to y2(not inclusive)
+     * @param y2 The final y value, where the last value of the output BufferedImage
+     *           is the pixel y-1
+     * @return The sub-image extracted from the input from x1 to x2(not inclusive)
+     *         and y2 to y2(not inclusive)
      */
-    public static BufferedImage splice(BufferedImage bi, int x1, int y1, int x2, int y2)
-    {
-        BufferedImage out = new BufferedImage(x2-x1+1,y2-y1+1,BufferedImage.TYPE_INT_RGB);
-        for(int j = y1; j <= y2; j++)
-        {
-            for(int i = x1; i <= x2; i++)
-            {
-                out.setRGB(i-x1,j-y1,bi.getRGB(i,j));
+    public static BufferedImage splice(BufferedImage bi, int x1, int y1, int x2, int y2) {
+        BufferedImage out = new BufferedImage(x2 - x1 + 1, y2 - y1 + 1, BufferedImage.TYPE_INT_RGB);
+        for (int j = y1; j <= y2; j++) {
+            for (int i = x1; i <= x2; i++) {
+                out.setRGB(i - x1, j - y1, bi.getRGB(i, j));
             }
         }
         return out;
     }
 
-    public static BufferedImage grayscale(BufferedImage bi)
-    {
+    public static BufferedImage grayscale(BufferedImage bi) {
         BufferedImage out = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_RGB);
-        for(int i = 0; i < bi.getWidth(); i++)
-        {
-            for(int j = 0; j < bi.getHeight(); j++)
-            {
-                Color c = new Color(bi.getRGB(i,j));
-                int val = (c.getRed() + c.getGreen() + c.getBlue())/3;
-                out.setRGB(i,j,new Color(val,val,val).getRGB());
+        for (int i = 0; i < bi.getWidth(); i++) {
+            for (int j = 0; j < bi.getHeight(); j++) {
+                Color c = new Color(bi.getRGB(i, j));
+                int val = (c.getRed() + c.getGreen() + c.getBlue()) / 3;
+                out.setRGB(i, j, new Color(val, val, val).getRGB());
             }
         }
         return out;
     }
 
-    public static BufferedImage histogramEqualization(BufferedImage bi)
-    {
+    public static BufferedImage histogramEqualization(BufferedImage bi) {
         BufferedImage out = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_RGB);
         double[] p = new double[256];
         int numPixels = bi.getWidth() * bi.getHeight();
-        for(int i = 0; i < bi.getWidth(); i++)
-        {
-            for(int j = 0; j < bi.getHeight(); j++)
-            {
-                int val = new Color(bi.getRGB(i,j)).getRed();
-                p[val] += 1.0/numPixels;
+        for (int i = 0; i < bi.getWidth(); i++) {
+            for (int j = 0; j < bi.getHeight(); j++) {
+                int val = new Color(bi.getRGB(i, j)).getRed();
+                p[val] += 1.0 / numPixels;
             }
         }
-        for(int i = 0; i < bi.getWidth(); i++)
-        {
-            for(int j = 0; j < bi.getHeight(); j++)
-            {
-                int val = (int)Math.floor((256-1)*sum(p,new Color(bi.getRGB(i,j)).getRed()));
-                out.setRGB(i,j,new Color(val,val,val).getRGB());
+        for (int i = 0; i < bi.getWidth(); i++) {
+            for (int j = 0; j < bi.getHeight(); j++) {
+                int val = (int) Math.floor((256 - 1) * sum(p, new Color(bi.getRGB(i, j)).getRed()));
+                out.setRGB(i, j, new Color(val, val, val).getRGB());
             }
         }
         return out;
     }
-    private static double sum(double[] vals, int end)
-    {
+
+    private static double sum(double[] vals, int end) {
         double out = 0;
-        for(int i = 0; i <= end; i++)
-        {
+        for (int i = 0; i <= end; i++) {
             out += vals[i];
         }
         return out;
     }
 
-    public static BufferedImage thresholdFilter(BufferedImage bi, int threshold)
-    {
+    public static BufferedImage thresholdFilter(BufferedImage bi, int threshold) {
         BufferedImage out = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_RGB);
-        for(int i = 0; i < bi.getWidth(); i++)
-        {
-            for(int j = 0; j < bi.getHeight(); j++)
-            {
-                int val = new Color(bi.getRGB(i,j)).getRed();
-                out.setRGB(i,j,(val >= threshold) ? 0xffffff : 0);
+        for (int i = 0; i < bi.getWidth(); i++) {
+            for (int j = 0; j < bi.getHeight(); j++) {
+                int val = new Color(bi.getRGB(i, j)).getRed();
+                out.setRGB(i, j, (val >= threshold) ? 0xffffff : 0);
             }
         }
         return out;
     }
 
-    public static BufferedImage thresholdFilter(BufferedImage bi)
-    {
+    public static BufferedImage thresholdFilter(BufferedImage bi) {
         double threshold = 0;
         int numPixels = bi.getWidth() * bi.getHeight();
-        for(int i = 0; i < bi.getWidth(); i++)
-        {
-            for(int j = 0; j < bi.getHeight(); j++)
-            {
-                int val = new Color(bi.getRGB(i,j)).getRed();
-                threshold += (double)val / numPixels;
+        for (int i = 0; i < bi.getWidth(); i++) {
+            for (int j = 0; j < bi.getHeight(); j++) {
+                int val = new Color(bi.getRGB(i, j)).getRed();
+                threshold += (double) val / numPixels;
             }
         }
-        return thresholdFilter(bi,(int)(0.5 * threshold));
+        return thresholdFilter(bi, (int) (0.5 * threshold));
+    }
+
+    public static Color averageColor(BufferedImage bi) {
+        double red = 0;
+        double green = 0;
+        double blue = 0;
+        int count = 0;
+        double scalar = 1.0 / (bi.getWidth() * bi.getHeight());
+        for (int i = 0; i < bi.getWidth(); i++) {
+            for (int j = 0; j < bi.getHeight(); j++) {
+                Color temp = new Color(bi.getRGB(i, j));
+                if (temp.getRed() != 255 && temp.getBlue() != 255 && temp.getGreen() != 255) {
+                    red += temp.getRed()/255.0;
+                    green += temp.getGreen()/255.0;
+                    blue += temp.getBlue()/255.0;
+                    count++;
+                }
+            }
+        }
+        red /= count;
+        green /= count;
+        blue /= count;
+        red *= 255;
+        green *= 255;
+        blue *= 255;
+        return new Color((int) red, (int) green, (int) blue);
     }
 }
