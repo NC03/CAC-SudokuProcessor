5026568018793072856344304521a28be0c096a1
N C <>
2019-10-09 12:59:13 -0700

    test

--- Test.java
+++ Test.java
@@ -16,6 +16,18 @@ public class Test
 		g.setColor(new Color(255,255,255));
 		g.fillRect(0,0,600,600);
 		g.setColor(new Color(0,0,0));
+		
+		
+		g.setFont(new Font("Arial",Font.BOLD,32));
+		for(int i = 0; i < 9; i++)
+		{
+		for(int j = 0; j < 9; j++)
+		{
+			g.drawString(""+(int)(Math.random()*10),i*600/9+10,j*600/9-10);
+			g.drawLine(0,i*600/9,600,i*600/9);
+			g.drawLine(i*600/9,0,i*600/9,600);
+		}
+		}
 		g.drawLine(200,0,200,600);
 		g.drawLine(400,0,400,600);
 		g.drawLine(0,200,600,200);
