687e8804f823f752f572e0e8c0982a91672feb45
N C <>
2019-10-18 21:55:09 -0700

    cleaning up more

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
+echo "Starting The Build"
+echo "Starting the Build" >> SudokuSolver.log
+
 echo "Compile"
-javac code/*.java -d bytecode/
+javac code/*.java -d bytecode/ >> SudokuSolver.log
 
 echo "Build Archive"
-jar -cfe SudokuSolver.jar GUI bytecode/*.class assets/*
+jar -cfe SudokuSolver.jar GUI bytecode/*.class assets/* >> SudokuSolver.log
 
 echo "Documentation"
-javadoc code/*.java -d docs/
+javadoc code/*.java -d docs/ >> SudokuSolver.log
